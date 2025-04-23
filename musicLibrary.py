0028d46427a4adf09767f3f8a9070c7702de8540
Pratvijikadra <pratvijikadra@gmail.com>
2025-04-23 18:23:07 +0530

    Add files via upload

---
+++ musicLibrary.py
@@ -0,0 +1,6 @@
+music = {
+    "look":"https://www.youtube.com/watch?v=rMfugUZD2dA",
+    "light":"https://www.youtube.com/watch?v=Nnop2walGmM",
+    "make":"https://www.youtube.com/watch?v=CdHz-pjage4",
+    "orange":"https://www.youtube.com/watch?v=6mr4cYJ7yew"
+}
